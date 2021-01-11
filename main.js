cad28d4846070c6e5cec4054a11173b11968c9a9
frazermills <77229450+frazermills@users.noreply.github.com>
2021-01-11 14:42:45 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-//test
+function estimatePi(n) {
+	let numPointCircle = 0;
+	let numPointTotal = 0;
+
+	for (let i = 0; i < n; i++) {
+		x = Math.random()
+		y = Math.random()
+		let distance = x**2 + y**2;
+		if (distance <= 1) {
+			numPointCircle += 1;
+		}
+		numPointTotal += 1;
+	}
+	return 4 * (numPointCircle / numPointTotal);
+}
